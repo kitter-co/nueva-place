245c5d14ca0138595d9229b3a89dea3c514c35fb
Drumming2008 <finn.reese@gmail.com>
2025-04-02 22:01:00 -0700

    e

--- google_auth.js
+++ google_auth.js
@@ -1,6 +1,5 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"
 
 const firebaseConfig = {
     apiKey: "AIzaSyCFyU3lKCbyZYnrqSDNQZ2WjQA8gYhWKkA",
@@ -10,56 +9,54 @@ const firebaseConfig = {
     messagingSenderId: "888072535263",
     appId: "1:888072535263:web:41d224e58b2f2ec1eefd6a",
     measurementId: "G-CHM59EXZ90"
-};
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
 
 // SIGN IN
 document.getElementById("sign-in").addEventListener("click", () => {
     signInWithPopup(auth, provider).then(result => {
-    const user = result.user;
+    const user = result.user
     if (!user.email.endsWith("@nuevaschool.org")) {
-        const errorDiv = document.getElementById("signin-error");
-        errorDiv.style.display = "block";
+        const errorDiv = document.getElementById("signin-error")
+        errorDiv.style.display = "block"
         setTimeout(() => {
-        errorDiv.style.display = "none";
-        }, 4000);
+            errorDiv.style.display = "none"
+        }, 4000)
 
-        signOut(auth);
+        signOut(auth)
 
     }
-    showUser(user.email);
-    });
-});
+    showUser(user.email)
+    })
+})
 
 //SIGN OUT
 document.getElementById("sign-out").addEventListener("click", () => {
     signOut(auth).then(() => {
-    hideUser();
-    });
-});
+    hideUser()
+    })
+})
 
 onAuthStateChanged(auth, user => {
     if (user && user.email.endsWith("@nueva.place")) {
-    showUser(user.email);
+    showUser(user.email)
     } else {
-    hideUser();
+    hideUser()
     }
-});
+})
 
 function showUser(email) {
-    document.getElementById("account-email").textContent = email;
-    document.getElementById("profile-img").style.display = "block";
-    document.getElementById("sign-in").style.display = "none";
-    document.getElementById("sign-out").style.display = "inline-block";
-    signedIn = true;
+    document.getElementById("account-email").innerText = email
+    document.getElementById("profile-img").style.display = "block"
+    document.getElementById("sign-in").style.display = "none"
+    signedIn = true
 }
 
 function hideUser() {
-    document.getElementById("profile-img").style.display = "none";
-    document.getElementById("sign-in").style.display = "block";
-    document.getElementById("sign-out").style.display = "none";
-    signedIn = false;
+    document.getElementById("profile-img").style.display = "none"
+    document.getElementById("sign-in").style.display = "block"
+    signedIn = false
 }
