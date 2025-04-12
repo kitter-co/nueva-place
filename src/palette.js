d614a1f9b55757f2889c96f2c6b45695975a1738
Drumming2008 <finn.reese@gmail.com>
2025-04-12 10:41:12 -0700

    error when white color selected

--- src/palette.js
+++ src/palette.js
@@ -27,7 +27,7 @@ function selectColor(rgb) {
     colorButtonsWrapper.classList.add("closed")
     colorButtonsWrapper.style.setProperty(
       "--selected-color",
-      rgb == "255, 255, 255" ? "#0004" : `rgb(${rgb})`
+      rgb == "255,255,255" ? "#0004" : `rgb(${rgb})`
     )
   }
 
