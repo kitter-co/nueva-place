fc4bcbd2746b7bb51a762e955828e1c325a28ab0
Drumming2008 <finn.reese@gmail.com>
2025-04-12 20:58:34 -0700

    dark mode simulator

--- src/auth.js
+++ src/auth.js
@@ -38,6 +38,7 @@ function signInSuccess(user) {
 id("profile-img").onclick = () => {
   id("account-wrapper").classList.toggle("menu-open")
   id("profile-button").classList.toggle("selected")
+
 }
 
 document.onmousedown = e => {
@@ -86,5 +87,38 @@ onAuthStateChanged(auth, user => {
     errorToast("You must use a @nuevaschool.org email address.")
   }
 })
+
+function updateTheme(theme) {
+  if (theme == "auto") {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.id = "dark"
+    } else {
+      document.documentElement.id = ""
+    }
+
+    localStorage.setItem("theme", "auto")
+  } else if (theme == "dark") {
+    document.documentElement.id = "dark"
+    localStorage.setItem("theme", "dark")
+  } else if (theme == "light") {
+    document.documentElement.id = ""
+    localStorage.setItem("theme", "light")
+  }
+
+  document.querySelector("#theme-selector > button.selected")?.classList.remove("selected")
+  document.querySelector(`#theme-selector > button[data-theme="${theme}"]`).classList.add("selected")
+}
+
+for (let i of document.querySelectorAll("#theme-selector > button")) {
+  i.onclick = () => {
+    updateTheme(i.dataset.theme)
+  }
+}
+
+if (localStorage.getItem("theme")) {
+  updateTheme(localStorage.getItem("theme"))
+} else {
+  updateTheme("auto")
+}
 
 export { isSignedIn }
