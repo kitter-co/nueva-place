1f33a8838028e943cda1a7fee67a5c64006708f2
Ayush Kumar <github@ayukmr.com>
2025-04-13 14:39:41 -0700

    Remove disable-transitions

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 import "./canvas.js"
 import "./toolbar.js"
 
-onload = () => {
-  document.body.classList.remove("disable-transitions")
-}
-
 function updateTheme(theme) {
   if (theme === "dark") {
     document.documentElement.id = "dark"
