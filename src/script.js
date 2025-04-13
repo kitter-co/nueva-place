a77605f675287a4a4bff89ae5976b6c615d28463
Ayush Kumar <github@ayukmr.com>
2025-04-12 21:47:07 -0700

    Remove theme selector

--- src/script.js
+++ src/script.js
@@ -2,4 +2,10 @@ import "./canvas.js"
 
 onload = () => {
   document.body.classList.remove("disable-transitions")
+
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.documentElement.id = "dark"
+  } else {
+    document.documentElement.id = ""
+  }
 }
