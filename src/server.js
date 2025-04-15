e157faa763f0b4d733d88066fa2cc10d41a82cf3
Ayush Kumar <github@ayukmr.com>
2025-04-14 20:33:51 -0700

    Revert server.js changes

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ import { errorToast, toast } from "./toast.js"
 import { id, hexToRGB, rgbToHex } from "./utils.js"
 import { clearCurrentColor, endCooldown, startCooldown } from "./palette.js"
 
-const socket = new WebSocket('wss://api.nueva.place')
+const AUTH = '5609854b-4c67-43f1-8d36-4967322f1074'
+const socket = new WebSocket(`wss://api.nueva.place/?auth=${AUTH}`)
 
 let queue = []
 
@@ -50,8 +51,8 @@ function interpret(data) {
         id("canvas").style.animation = "fade-in 0.2s ease-out forwards"
 
         draw(true)
-        if (location.hash) {
-          let viewportData = location.hash.slice(1).split(",").map(Number)
+        if (location.search) {
+          let viewportData = location.search.slice(1).split(",").map(Number)
           if (viewportData.length === 4 && !viewportData.some(isNaN)) {
             loadViewportDataArray(viewportData)
           } else {
